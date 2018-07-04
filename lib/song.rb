21d6863dee9c5767121426b14ae616e56410ab64
Priscilla H <priscillamhuynh@gmail.com>
2018-07-04 12:46:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,7 +54,7 @@ class Song
    end
 
    def self.create_from_filename(filename_format)
-     song=self.new_from_filename
+     song=self.new_from_filename(filename_format)
      song.save
    end
 
