d77be5e453bbbab0ca9b81fa1ba917d02ac5fa12
Priscilla H <priscillamhuynh@gmail.com>
2018-07-04 09:03:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   end
 
   def self.find_by_name(string_name)
-    self.all.detect {|song| if song == string_name}
+    self.all.detect {|song| if song.name == string_name}
   end
 
 end
